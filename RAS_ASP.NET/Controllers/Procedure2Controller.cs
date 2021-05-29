18d1601c731fb7a2cb1849a2e4f4e911683e18fb
vladi <vladislavvasilenko2001@gmail.com>
2021-05-29 21:23:23 +0300

    kinch (v13)

---
+++ RAS_ASP.NET/Controllers/Procedure2Controller.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RAS_ASP.NET.Models;
+using System.Collections.Generic;
+using WebApplication.Controllers;
+using WebApplication.Data.DAO;
+using WebApplication.Data.Entities;
+namespace RAS_ASP.NET.Controllers
+{
+    public class Procedure2Controller : Controller
+    {
+
+        // GET: Procedure2Controller/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Procedure2Controller/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DishEntity entity)
+        {
+            try
+            {
+                //public IList<ComponentEntity> Component;
+                using (NHibernate.ISession session = NHibernateManager.OpenSession())
+                {
+                    var factory = new DAOFactory(session);
+                    factory.GetDishDAO().Add((double)entity.Price, (double)entity.Weight, 
+                        (int)entity.Cuisine.ID, entity.Component.Count);
+                }
+                return View(entity);
+            }
+            catch
+            {
+                return View(entity);
+            }
+        }
+    }
+}
