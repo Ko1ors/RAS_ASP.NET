acf03fde0a1c87ebc973c8330973ce9c6b161a1a
vladi <vladislavvasilenko2001@gmail.com>
2021-05-30 17:37:09 +0300

    v14

---
+++ RAS_ASP.NET/Controllers/Procedure3Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RAS_ASP.NET.Models;
+using System.Collections.Generic;
+using WebApplication.Controllers;
+using WebApplication.Data.DAO;
+using WebApplication.Data.Entities;
+
+namespace RAS_ASP.NET.Controllers
+{
+    public class Procedure3Controller : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Procedure2Controller/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CuisineEntity entity)
+        {
+            try
+            {
+                using (NHibernate.ISession session = NHibernateManager.OpenSession())
+                {
+                    var factory = new DAOFactory(session);
+                    factory.GetCuisineDAO().GetTotalPrice((int)entity.ID);
+                }
+                return View(entity);
+            }
+            catch
+            {
+                return View(entity);
+            }
+        }
+    }
+}
