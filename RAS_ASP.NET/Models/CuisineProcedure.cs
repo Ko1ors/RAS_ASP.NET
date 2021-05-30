af2dfed4cd68c79307ed64e5742ee54192ef05d2
vladi <vladislavvasilenko2001@gmail.com>
2021-05-30 18:18:17 +0300

    v15

---
+++ RAS_ASP.NET/Models/CuisineProcedure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RAS_ASP.NET.Models
+{
+    public class CuisineProcedure : CuisineTotalRecord
+    {
+        public int ID { get; set; }
+    }
+}
