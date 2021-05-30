7b9102f624f34d889bc4ee1eb9bdfb0982503886
Ko1ors <kolorsnik181200@gmail.com>
2021-05-30 18:16:04 +0300

    Procedure 2 update

--- RAS_ASP.NET/Controllers/Procedure2Controller.cs
+++ RAS_ASP.NET/Controllers/Procedure2Controller.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RAS_ASP.NET.Models;
-using System.Collections.Generic;
 using WebApplication.Controllers;
 using WebApplication.Data.DAO;
-using WebApplication.Data.Entities;
 namespace RAS_ASP.NET.Controllers
 {
     public class Procedure2Controller : Controller
@@ -13,26 +10,26 @@ namespace RAS_ASP.NET.Controllers
         // GET: Procedure2Controller/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new DishProcedure());
         }
 
         // POST: Procedure2Controller/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(DishEntity entity)
+        public ActionResult Create(DishProcedure entity)
         {
             try
             {
                 using (NHibernate.ISession session = NHibernateManager.OpenSession())
                 {
-                    var factory = new DAOFactory(session);
-                    factory.GetDishDAO().Add(entity.Price, entity.Weight, 
-                        (int)entity.Cuisine.ID, entity.Component.Count);
+                    entity.Component = new DAOFactory(session).GetDishDAO().Add(entity.Price, entity.Weight, (int)entity.Cuisine.ID, entity.ComponentCount);
+                    entity.Status = "Successful";
                 }
                 return View(entity);
             }
             catch
             {
+                entity.Status = "Error";
                 return View(entity);
             }
         }
---
+++ RAS_ASP.NET/Models/DishProcedure.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WebApplication.Data.Entities;
+
+namespace RAS_ASP.NET.Models
+{
+    public class DishProcedure : DishEntity
+    {
+        public int ComponentCount { get; set; }
+
+        public string Status { get; set; }
+
+        public DishProcedure()
+        {
+            Component = new List<ComponentEntity>();
+        }
+    }
+}
